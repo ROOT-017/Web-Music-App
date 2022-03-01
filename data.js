f92b4285c2d2d7055501e9ca790ad2e6aca2bded
NKWETACHA TERENCE NGWEN <nkwetachaterence17@gmail.com>
2022-03-01 18:50:00 +0100

    Data added

---
+++ data.js
@@ -0,0 +1,58 @@
+let songs = [{
+        name: 'Astronaut In The Ocean',
+        path: 'assets/musics/Masked Wolf - Astronaut In The Ocean (Official Music Video).mp3',
+        artist: 'Masked Wolf',
+        cover: 'assets/images/cover 1.jpg'
+    },
+    {
+        name: 'Lonely',
+        path: 'assets/musics/Akon - Lonely (Official Music Video).mp3',
+        artist: 'Akon',
+        cover: 'assets/images/cover 2.jpg'
+    },
+    {
+        name: 'Down In the DM',
+        path: 'assets/musics/Yo Gotti - Down In the DM (Official Music Video).mp3',
+        artist: 'Yo Gotti',
+        cover: 'assets/images/cover 3.jpg'
+    },
+    {
+        name: 'The Way I Are Dance With Somebody',
+        path: 'assets/musics/Bebe_rexa_The Way I Are Dance With Somebody -ft_lil_wagne.mp3',
+        artist: 'Bebe_rexa ft_lil_wagne',
+        cover: 'assets/images/cover 4.jpg'
+    },
+    {
+        name: 'Halo',
+        path: 'assets/musics/Beyoncé - Halo.mp3',
+        artist: 'Beyoncé',
+        cover: 'assets/images/cover 5.jpg'
+    },
+    {
+        name: 'Let It Reign',
+        path: 'assets/musics/KB - Let It Reign (feat. Bizzle) _Official Audio_ ( 256kbps cbr ).mp3',
+        artist: 'KB feat. Bizzle',
+        cover: 'assets/images/cover 6.jpg'
+    },
+    {
+        name: 'Young Dumb & Broke',
+        path: 'assets/musics/Young Dumb & Broke _ Khalid _ Lyrics.mp3',
+        artist: 'Khalid',
+        cover: 'assets/images/cover 7.jpg'
+    },
+    {
+        name: 'See_you_again',
+        path: 'assets/musics/Wiz_khalifa_see_you_again.mp3',
+        artist: 'Wiz_khalifa',
+        cover: 'assets/images/cover 8.jpg'
+    },
+    {
+        name: 'At My Worst',
+        path: 'assets/musics/Pink Sweat$ - At My Worst (feat. Kehlani) _Official Video_.mp3',
+        artist: 'Pink Sweat$ feat. Kehlani',
+        cover: 'assets/images/cover 9.jpg'
+    }
+
+]
+
+//Songs added
