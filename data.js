0491042e5e7f3972e69516f31bf49d43fd2ebb81
NKWETACHA TERENCE <78331614+ROOT-017@users.noreply.github.com>
2022-05-19 17:02:17 +0100

    All paths made absolute

--- data.js
+++ data.js
@@ -1,56 +1,56 @@
 let songs = [{
         name: 'Astronaut In The Ocean',
-        path: 'assets/musics/Masked Wolf - Astronaut In The Ocean (Official Music Video).mp3',
+        path: 'Assets/musics/Masked Wolf - Astronaut In The Ocean (Official Music Video).mp3',
         artist: 'Masked Wolf',
-        cover: 'assets/images/cover 1.jpg'
+        cover: 'Assets/Images/cover 1.jpg'
     },
     {
         name: 'Lonely',
-        path: 'assets/musics/Akon - Lonely (Official Music Video).mp3',
+        path: 'Assets/musics/Akon - Lonely (Official Music Video).mp3',
         artist: 'Akon',
-        cover: 'assets/images/cover 2.jpg'
+        cover: 'Assets/Images/cover 2.jpg'
     },
     {
         name: 'Down In the DM',
-        path: 'assets/musics/Yo Gotti - Down In the DM (Official Music Video).mp3',
+        path: 'Assets/musics/Yo Gotti - Down In the DM (Official Music Video).mp3',
         artist: 'Yo Gotti',
-        cover: 'assets/images/cover 3.jpg'
+        cover: 'Assets/Images/cover 3.jpg'
     },
     {
         name: 'The Way I Are Dance With Somebody',
-        path: 'assets/musics/Bebe_rexa_The Way I Are Dance With Somebody -ft_lil_wagne.mp3',
+        path: 'Assets/musics/Bebe_rexa_The Way I Are Dance With Somebody -ft_lil_wagne.mp3',
         artist: 'Bebe_rexa ft_lil_wagne',
-        cover: 'assets/images/cover 4.jpg'
+        cover: 'Assets/Images/cover 4.jpg'
     },
     {
         name: 'Halo',
-        path: 'assets/musics/Beyoncé - Halo.mp3',
+        path: 'Assets/musics/Beyoncé - Halo.mp3',
         artist: 'Beyoncé',
-        cover: 'assets/images/cover 5.jpg'
+        cover: 'Assets/Images/cover 5.jpg'
     },
     {
         name: 'Let It Reign',
-        path: 'assets/musics/KB - Let It Reign (feat. Bizzle) _Official Audio_ ( 256kbps cbr ).mp3',
+        path: 'Assets/musics/KB - Let It Reign (feat. Bizzle) _Official Audio_ ( 256kbps cbr ).mp3',
         artist: 'KB feat. Bizzle',
-        cover: 'assets/images/cover 6.jpg'
+        cover: 'Assets/Images/cover 6.jpg'
     },
     {
         name: 'Young Dumb & Broke',
-        path: 'assets/musics/Young Dumb & Broke _ Khalid _ Lyrics.mp3',
+        path: 'Assets/musics/Young Dumb & Broke _ Khalid _ Lyrics.mp3',
         artist: 'Khalid',
-        cover: 'assets/images/cover 7.jpg'
+        cover: 'Assets/Images/cover 7.jpg'
     },
     {
         name: 'See_you_again',
-        path: 'assets/musics/Wiz_khalifa_see_you_again.mp3',
+        path: 'Assets/musics/Wiz_khalifa_see_you_again.mp3',
         artist: 'Wiz_khalifa',
-        cover: 'assets/images/cover 8.jpg'
+        cover: 'Assets/Images/cover 8.jpg'
     },
     {
         name: 'At My Worst',
-        path: 'assets/musics/Pink Sweat$ - At My Worst (feat. Kehlani) _Official Video_.mp3',
+        path: 'Assets/musics/Pink Sweat$ - At My Worst (feat. Kehlani) _Official Video_.mp3',
         artist: 'Pink Sweat$ feat. Kehlani',
-        cover: 'assets/images/cover 9.jpg'
+        cover: 'Assets/Images/cover 9.jpg'
     }
 
 ]
